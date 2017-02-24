9c6962b1ab4a726bd661a312fd97eebfb63ab7af
Markus Peterson <marcus17777@gmail.com>
2017-02-24 20:13:03 +0200

    Added deployment system.

---
+++ config/deploy.rb
@@ -0,0 +1,69 @@
+# config valid only for current version of Capistrano
+lock "3.7.2"
+
+set :application, "welcome"
+set :repo_url, "git@github.com:team-kektus/welcome.git"
+
+user_path = '/home/kektus'
+project_path = "#{user_path}/welcome"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+set :deploy_to, project_path
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+set :format, :pretty
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+
+namespace :deploy do
+
+  task :add_deps do
+    on roles(:app) do
+      execute "cd #{project_path} && mv -t current/ releases/node_modules releases/bower_components"
+    end
+  end
+
+  task :remove_deps do
+    on roles(:app) do
+      execute "cd #{project_path}/current/ && mv -t ../ node_modules bower_components"
+    end
+  end
+
+
+  task :restart_app do
+    on roles(:app) do
+      execute "pm2 startOrRestart #{user_path}/pm2_conf/welcome.ecosystem.js"
+    end
+  end
+
+  before :starting, :remove_deps
+  after :publishing, :add_deps
+  after :finished, :restart_app
+end
