4c6dd629e8a113c718c8684dec25a7a3d578bbc6
Kristjan Puusepp <kristjanps96@gmail.com>
2017-02-28 21:50:14 +0200

    register href added

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,6 +1,17 @@
 class HomeController {
   constructor() {
     console.log('Initializing HomeController');
+    this.model = {username: "", password: ""}
+
+  }
+  consolelog() {
+      console.log("Clicked button");
+      if (this.model.username == "tere" && this.model.password == "1234 ") {
+        window.location.href="http://kodu.ut.ee/~qristjan/protot%C3%BC%C3%BCp/profile.html";
+      }
+      else (
+        console.log("Tere")
+      )
   }
 }
 
